d00f590322cf65bb40f0451168cb3fedf761e374
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2013-02-16 16:39:18 +0100

    Finder rewrite

--- src/Woodling/Finder.php
+++ src/Woodling/Finder.php
@@ -1,5 +1,11 @@
 <?php namespace Woodling;
 
+/**
+ * Finder loads Blueprints for you.
+ *
+ * @package Woodling
+ */
+
 class Finder
 {
 
@@ -13,6 +19,20 @@ class Finder
     );
 
     /**
+     * Adds paths to the default search array.
+     *
+     * Paths can be relative to your root file or absolute. Takes a string or an array of
+     * paths.
+     *
+     * <code>
+     *     $finder->addPaths('src/Acme/DemoBundle/Tests/blueprints');
+     *
+     *     $finder->addPaths(array(
+     *         '/home/users/mindaugas/blueprints',
+     *         'app/tests/blueprints'
+     *     ));
+     * </code>
+     *
      * @param string|array $paths String or array of paths to expand search with
      */
     public function addPaths($paths)
@@ -25,6 +45,49 @@ class Finder
         foreach($paths as $path)
         {
             $this->searchPaths[] = $path;
+        }
+    }
+
+    /**
+     * Attempts to load files specified in `$this->searchPaths`.
+     *
+     * By default, it will attempt to load the following files:
+     *
+     * - applications/tests/blueprints/*.php
+     * - applications/tests/blueprints.php
+     * - app/tests/blueprints/*.php
+     * - app/tests/blueprints.php
+     * - tests/blueprints/*.php
+     * - tests/blueprints.php
+     *
+     * It iterates over `$this->searchPaths` array and appends `.php` to each path, trying
+     * to load it as a file. Afterwards it loads this path as a folder and iterates over
+     * each item in it and if PHP file is found, loads it.
+     */
+    public function findBlueprints()
+    {
+        foreach($this->searchPaths as $path)
+        {
+            // Try to load $path.php
+            $file = rtrim($path, '/') . '.php';
+            if (file_exists($file))
+            {
+                require_once($file);
+            }
+
+            // Load php files in path
+            if (is_dir($path))
+            {
+                foreach (new \DirectoryIterator($path) as $node)
+                {
+                    $ext = strtolower(pathinfo($node->getPathName(), PATHINFO_EXTENSION));
+
+                    if ($node->isFile() && $ext === 'php')
+                    {
+                        require_once($node->getPathName());
+                    }
+                }
+            }
         }
     }
 
