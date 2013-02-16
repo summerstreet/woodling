146889a2f35272da7808650775177856f806688c
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2013-02-16 14:38:19 +0100

    Refactoring Finder, adding search paths

--- src/Woodling/Finder.php
+++ src/Woodling/Finder.php
@@ -4,45 +4,16 @@ class Finder
 {
 
     /**
-     * @var array Paths to check
+     * @var array Default search paths (last node serves as both file and folder name)
      */
-    protected $paths = array(
-        'application/tests/',
-        'app/tests/',
-        'tests/'
+    public $searchPaths = array(
+        'applications/tests/blueprints',
+        'app/tests/blueprints',
+        'tests/blueprints'
     );
 
     /**
-     * @var string Default filename
-     */
-    protected $filename = 'blueprints.php';
-
-    /**
-     * @return string
-     */
-    public function getFilename()
-    {
-        return $this->filename;
-    }
-
-    /**
-     * @param array $paths
-     */
-    public function setPaths($paths)
-    {
-        $this->paths = $paths;
-    }
-
-    /**
-     * @return array
-     */
-    public function getPaths()
-    {
-        return $this->paths;
-    }
-
-    /**
-     * @param array|string $paths
+     * @param string|array $paths String or array of paths to expand search with
      */
     public function addPaths($paths)
     {
@@ -53,75 +24,7 @@ class Finder
 
         foreach($paths as $path)
         {
-            $this->paths[] = $path;
-        }
-    }
-
-    /**
-     * @param string $dir
-     * @return bool
-     */
-    public function isDir($dir)
-    {
-        return is_dir($dir);
-    }
-
-    /**
-     * @param string $file
-     * @return bool
-     */
-    public function fileExists($file)
-    {
-        return file_exists($file);
-    }
-
-    /**
-     * @param string $file
-     * @return mixed
-     */
-    public function loadFile($file)
-    {
-        return require_once($file);
-    }
-
-    /**
-     * @param string $path
-     */
-    public function loadByFilename($path)
-    {
-        $filepath = rtrim($path, '/').'/'.$this->filename;
-        if ($this->fileExists($filepath))
-        {
-            $this->loadFile($filepath);
-        }
-    }
-
-    /**
-     * @param string $path
-     */
-    public function loadInPath($path)
-    {
-        foreach (new \DirectoryIterator($path) as $node)
-        {
-            $ext = strtolower(pathinfo($node->getPathName(), PATHINFO_EXTENSION));
-
-            if ($node->isFile() && $ext === 'php')
-            {
-                $this->loadFile($node->getPathName());
-            }
-        }
-    }
-
-    public function findBlueprints()
-    {
-        $paths = $this->getPaths();
-
-        foreach($paths as $path)
-        {
-            if ( ! $this->isDir($path)) continue;
-
-            $this->loadByFilename($path);
-            $this->loadInPath($path);
+            $this->searchPaths[] = $path;
         }
     }
 
